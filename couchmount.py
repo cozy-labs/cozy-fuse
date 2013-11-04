d2217b9a5dcdee58c572829b9c17d804614a3941
poupotte <bellot.zoe@gmail.com>
2013-11-04 16:33:16 +0100

    update with speration between binary and metadata

--- couchmount.py
+++ couchmount.py
@@ -16,10 +16,17 @@ import os
 import stat
 import sys
 import time
+import base64
+import thread
 try:
     import simplejson as json
 except ImportError:
     import json # Python 2.6
+try:
+    import _thread # Python 3
+except ImportError:
+    import thread
+from threading import Thread
 from urllib import quote, unquote    
 
 fuse.fuse_python_api = (0, 2)
@@ -42,19 +49,100 @@ def _normalize_path(path):
     return u'/'.join([part for part in path.split(u'/') if part != u''])
 
 def _recover_path(db): 
-    res = db.view("remote/all")
+    res = db.view("device/all")
     if not res:
-        time.sleep(1)
+        time.sleep(5)
         return _recover_path(db)
     else:
-        for res in db.view("remote/all"):
-            if not res.value["folder"]:
-                time.sleep(1)
+        for device in res:
+            if not device.value["folder"]:
+                time.sleep(5)
                 return _recover_path(db)
             else:
-                return res.value['folder']
+                return device.value['folder']
 
+def _replicate_to_local(self, ids):
+    target = 'http://%s:%s@localhost:5984/cozy' % (self.username, self.password)
+    url = self.urlCozy.split('/')
+    source = "https://%s:%s@%s/cozy" % (self.loginCozy, self.passwordCozy, url[2])
+    self.rep = self.server.replicate(source, target, doc_ids=ids)
 
+def _replicate_from_local(self, ids):
+    source = 'http://%s:%s@localhost:5984/cozy' % (self.username, self.password)
+    url = self.urlCozy.split('/')
+    target = "https://%s:%s@%s/cozy" % (self.loginCozy, self.passwordCozy, url[2])
+    self.rep = self.server.replicate(source, target, doc_ids=ids)
+
+def _deleteFile(line, self):
+    try:
+        # Remove binary if a file has been deleted
+        if line['deleted'] and line['deleted'] is True:
+            try:
+                binary = self.ids[line['doc']['_id']][0]
+                if self.db[binary]:
+                    self.db.delete(self.db[binary])
+                    _replicate_to_local(self, [binary])
+                return True
+            except (Exception):
+                return True
+    except (KeyError):
+        return False
+
+def _addFile(line, self):  
+    try:
+        # Add binary if File has been added
+        if line['doc']['_rev'][0] is "1" and line['doc']['_rev'][1] is '-':
+            id_doc = line['doc']['_id']
+            doc = self.db[id_doc]
+            if doc['docType'] == 'File':
+                try:
+                    if self.ids[id_doc]:
+                        return True
+                except (KeyError):
+                    self.ids[id_doc] = ["", ""]
+                    return True
+        else:
+            return False
+    except (Exception):
+        return True
+
+def _updateFile(line, self): 
+    try: 
+        id_doc = line['doc']['_id']
+        doc = self.db[id_doc]
+        if doc['docType'] == 'File':
+            binary = doc['binary']['file']
+            if binary['rev'] != self.ids[id_doc][1]:
+                self.ids[id_doc] = [binary['id'], binary['rev']]
+                _replicate_to_local(self, [binary['id']])
+                return True
+    except (Exception):
+        return False   
+
+def _isDevice(line):
+    try:
+        if line['doc']['docType'] != "Device":                    
+            return False
+        else:
+            return True
+    except (Exception):
+        return False
+
+def _changeFile(self): 
+    files = self.db.view("file/all")
+    self.ids = {}
+    for res in files:
+        binary = res.value["binary"]["file"]
+        self.ids[res.id] = [binary["id"], binary["rev"]]
+    changes = self.db.changes(feed='continuous', heartbeat='1000', since=self.device['change'], include_docs=True)
+    for line in changes:
+        if not _isDevice(line):
+            self.device['change'] = line['seq'] + 1
+            self.db.save(self.device)
+            id_doc = line['doc']
+            if not _deleteFile(line, self):
+                if not _addFile(line, self):  
+                    _updateFile(line, self)     
 
 
 class CouchFSDocument(fuse.Fuse):
@@ -62,9 +150,26 @@ class CouchFSDocument(fuse.Fuse):
         fuse.Fuse.__init__(self, *args, **kwargs)        
         self.fuse_args.mountpoint = mountpoint
         db_uri = uri
-        self.db = Database(db_uri)
+        self.server = Server('http://localhost:5984/')
+        f = open('/etc/cozy/couchdb.login')
+        lines = f.readlines()
+        f.close()
+        self.username = lines[0].strip()
+        self.password = lines[1].strip()
+        # Add credentials
+        self.server.resource.credentials = (self.username, self.password)
+        self.db = self.server['cozy']
         self.currentFile = ""
-
+        res = self.db.view("device/all")
+        for device in res:
+            self.urlCozy = device.value['url']
+            self.passwordCozy = device.value['password']
+            self.loginCozy = device.value['login']
+            self.device = device.value
+        try:
+            Thread(target=_changeFile, args=[self]).start()
+        except Exception, errtxt:
+            print errtxt
        
     def get_dirs(self):
         """
@@ -72,17 +177,18 @@ class CouchFSDocument(fuse.Fuse):
         """
         dirs = {}
         for res in self.db.view("folder/all"):
-            att = res.value["slug"]
-            path = att[1:]
+            att = res.value["path"] + '/' + res.value["name"]
+            if len(att) != 0:
+                att = att[1:]
             parents = [u'']
             for name in att.split('/'):
                 if name != '':
                     filenames = dirs.setdefault(u'/'.join(parents[1:]), set())
                     filenames.add(name)
                     parents.append(name)
-                    dirs.setdefault(u''+path, set())
+                    dirs.setdefault(u''+ att, set())
         for res in self.db.view("file/all"):
-            att = res.value["slug"]
+            att = res.value["path"] + '/' + res.value["name"]
             parents = [u'']
             for name in att.split('/'):
                 if name != '':
@@ -117,10 +223,12 @@ class CouchFSDocument(fuse.Fuse):
             else:
                 exist = "false"
                 for res in self.db.view("file/all"):
-                    if res.value["slug"] == '/' + path:
+                    res = res.value
+                    if res["path"] + "/" + res["name"] == '/' + path:
                         exist = "true"
-                        att = self.db[res.id].get('_attachments', {})
-                        data = att["thumb"]
+                        bin = res["binary"]["file"]["id"]
+                        att = self.db[bin].get('_attachments', {})
+                        data = att["file"]
                         st.st_mode = stat.S_IFREG | 0664
                         st.st_nlink = 1
                         st.st_size = data['length']
@@ -159,8 +267,10 @@ class CouchFSDocument(fuse.Fuse):
         path = _normalize_path(path)
         try:
             for res in self.db.view("file/all"):
-                if res.value["slug"] == '/' + path:
-                    data = self.db.get_attachment(res.id, "thumb")
+                res = res.value
+                if res["path"] + "/" + res["name"] == '/' + path:
+                    bin = res["binary"]["file"]["id"]
+                    data = self.db.get_attachment(bin, "file")
                     if data == None:
                         return ''
                     else:
@@ -187,7 +297,8 @@ class CouchFSDocument(fuse.Fuse):
         path = _normalize_path(path)
         try:
             for res in self.db.view("file/all"):
-                if res.value["slug"] == '/' + path:   
+                res = res.value
+                if res["path"] + "/" + res["name"] == '/' + path:   
                     self.currentFile = self.currentFile + buf
                     return len(buf)
         except (KeyError, ResourceNotFound):
@@ -204,11 +315,12 @@ class CouchFSDocument(fuse.Fuse):
             to an open file: all file descriptors are closed and 
             all memory mappings are unmapped.
         """
-        print fuse_file_info
         if self.currentFile != "": 
             for res in self.db.view("file/all"):
-                if res.value["slug"] == path:   
-                    self.db.put_attachment(self.db[res.id], self.currentFile, filename="thumb")
+                res = res.value
+                if res["path"] + "/" + res["name"] == path:   
+                    bin = res["binary"]["file"]["id"]
+                    self.db.put_attachment(self.db[bin], self.currentFile, filename="file")
             self.currentFile = ""
 
     def mknod(self, path, mode, dev):
@@ -219,13 +331,17 @@ class CouchFSDocument(fuse.Fuse):
             dev: if the file type is S_IFCHR or S_IFBLK, dev specifies the major
                  and minor numbers of the newly created device special file
         """
-        #path = _normalize_path(path)
         partialPaths = path.split('/')
         name = partialPaths[len(partialPaths) -1]
-        filePath = path.replace('/' + name, '')
-        newFile = {"name": name, "path": filePath, "slug":path, "docType": "File"}
+        filePath = path[:-(len(name)+1)]
+        newBin = {"docType": "Binary"}
+        id_bin = self.db.create(newBin)
+        self.db.put_attachment(self.db[id_bin], '', filename="file")
+        rev = self.db[id_bin]["_rev"]
+        newFile = {"name": name, "path": filePath, "binary":{"file": {"id": id_bin, "rev": rev}}, "docType": "File"}
         id_doc = self.db.create(newFile)
-        self.db.put_attachment(self.db[id_doc], '', filename="thumb")
+        self.ids[id_doc]= [id_bin, rev]
+        _replicate_from_local(self, [id_bin])
 
     def unlink(self, path):
         """
@@ -239,8 +355,11 @@ class CouchFSDocument(fuse.Fuse):
         else:
             dirname, filename = parts
         for res in self.db.view("file/all"):
-            if res.value["slug"] == '/' + path:
-                self.db.delete(self.db[res.id])
+            res = res.value
+            if res["path"] + "/" + res["name"] == '/' + path:
+                bin = res["binary"]["file"]["id"]
+                self.db.delete(self.db[bin])
+                self.db.delete(self.db[res["_id"]])
 
     def truncate(self, path, size):
         """
@@ -248,12 +367,6 @@ class CouchFSDocument(fuse.Fuse):
             path {string}: file path
             size {integer}: new file size
         """
-        for res in self.db.view("file/all"):
-            if res.value["slug"] == path:
-                f = open(path)
-                path = _normalize_path(path)
-                self.db.put_attachment(self.db[res.id], f, filename="thumb")
-                f.close()
         return 0
 
     def utime(self, path, times):
@@ -274,8 +387,8 @@ class CouchFSDocument(fuse.Fuse):
         #path = _normalize_path(path)
         partialPaths = path.split('/')
         name = partialPaths[len(partialPaths) -1]
-        folderPath = path.replace('/' + name, '')
-        id_doc = self.db.create({"name": name, "path": folderPath, "slug": path, "docType": "Folder"})
+        folderPath = path[:-(len(name)+1)]
+        id_doc = self.db.create({"name": name, "path": folderPath, "docType": "Folder"})
         return 0
 
     def rmdir(self, path):
@@ -284,8 +397,10 @@ class CouchFSDocument(fuse.Fuse):
             path {string}: diretory path
         """
         for res in self.db.view("folder/all"):
-            if res.value["slug"] == path:
-                self.db.delete(self.db[res.id])
+            res = res.value
+            fullPath = res["path"] + "/" + res["name"]
+            if res["path"] + "/" + res["name"] == path:
+                self.db.delete(self.db[res['_id']])
                 return 0
 
     def rename(self, pathfrom, pathto):
@@ -294,23 +409,43 @@ class CouchFSDocument(fuse.Fuse):
             pathfrom {string}: old path
             pathto {string}: new path
         """
-        for res in self.db.view("file/all"):
-            if res.value["slug"] == pathfrom:
-                doc = res.value
+        for doc in self.db.view("file/all"):
+            doc = doc.value
+            if doc["path"] + "/" + doc["name"] == pathfrom:
                 partialPaths = pathto.split('/')
                 name = partialPaths[len(partialPaths) -1]
-                filePath = pathto.replace('/' + name, '')
-                doc.update({"slug": pathto, "name": name, "path": filePath})
-                self.db.save(doc)        
-        for res in self.db.view("folder/all"):
-            if res.value["slug"] == pathfrom:
-                doc = res.value
+                filePath = pathto[:-(len(name)+1)]
+                doc.update({"name": name, "path": filePath})
+                self.db.save(doc) 
+        for doc in self.db.view("folder/all"):
+            doc = doc.value
+            fullPath = doc["path"] + "/" + doc["name"]
+            if fullPath == pathfrom:
                 partialPaths = pathto.split('/')
                 name = partialPaths[len(partialPaths) -1]
-                filePath = pathto.replace('/' + name, '')
-                doc.update({"slug": pathto, "name": name, "path": filePath})
+                filePath = pathto[:-(len(name)+1)]
+                doc.update({"name": name, "path": filePath})
+                # Rename all subfiles
+                for res in self.db.view("file/byFolder", key=fullPath):
+                    pathfrom = res.value['path'] + '/' + res.value['name']
+                    pathto = filePath + '/' + name + '/' + res.value['name']
+                    self.rename(pathfrom, pathto)
+                for res in self.db.view("folder/byFolder", key=fullPath):
+                    pathfrom = res.value['path'] + '/' + res.value['name']
+                    pathto = filePath + '/' + name + '/' + res.value['name']
+                    self.rename(pathfrom, pathto)
                 self.db.save(doc)
                 return 0
+
+    """def chown(self, path, user, group):
+        print("chown %s %s %s" % (path,user,group))
+        return os.chown(self.p(path), user, group)
+
+    def chmod(self, path, mode):
+        print("chmod %s %s" % (path,mode))
+        return os.chmod(self.p(path), mode)
+    """
+
 
     def fsync(self, path, isfsyncfile):
         """
@@ -351,31 +486,36 @@ class CouchFSDocument(fuse.Fuse):
         return st
 
 
+def _init():
+    try:
+        server = Server('http://localhost:5984/')
+        # Read file
+        f = open('/etc/cozy/couchdb.login')
+        lines = f.readlines()
+        f.close()
+        username = lines[0].strip()
+        password = lines[1].strip()
+        # Add credentials
+        server.resource.credentials = (username, password)
+        try:
+            db = server['cozy']
+        except Exception, e:
+            db = server.create('cozy')
+            # add user
+        if '_design/device' not in db:
+           db["_design/device"] = {"views": {"all": {"map": "function (doc) {\n" + 
+                "    if (doc.docType === \"Device\") {\n        emit(doc.id, doc) \n    }\n}"}}} 
+        folder = _recover_path(db)
+        fs = CouchFSDocument(folder, 'http://localhost:5984/cozy')
+        fs.parse(errex=1)
+        fs.main()
+    except Exception, e:
+        time.sleep(5)
+        _init()
+
 def main():
     args = sys.argv[1:]
-    if len(args) not in (0,1):
-        print "CouchDB FUSE Connector: Allows you to browse the _attachments of"
-        print " any CouchDB document on your own filesystem!"
-        print
-        print "Usage: python couchmount.py [-d]"
-        print
-        print "Unmount with : fusermount -u <mount-point>"
-        sys.exit(-1)
-    server = Server('http://localhost:5984/')
-    try:
-        db = server['cozy']
-    except Exception, e:
-        db = server.create('cozy')
-    if '_design/remote' not in db:
-       db["_design/remote"] = {"views": {"all": {"map": "function (doc) {\n    if (doc.docType === \"Remote\") {\n        emit(doc.id, doc) \n    }\n}"}}} 
-    if '_design/file' not in db:
-        db["_design/file"] = {"views": {"all": {"map": "function (doc) {\n    if (doc.docType === \"File\") {\n        emit(doc.id, doc) \n    }\n}"}}}
-    if '_design/folder' not in db:
-        db["_design/folder"] = {"views": {"all": {"map": "function (doc) {\n    if (doc.docType === \"Folder\") {\n        emit(doc.id, doc) \n    }\n}"}}}
-    folder = _recover_path(db)
-    fs = CouchFSDocument(folder, 'http://localhost:5984/cozy')
-    fs.parse(errex=1)
-    fs.main()
+    _init()   
 
 if __name__ == '__main__':
     main()
