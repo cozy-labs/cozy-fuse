fa4f26f6e6bec83fe238050485da6eb5a9418445
Frank Rousseau <frank.rousseau@cozycloud.cc>
2013-11-27 20:42:07 +0100

    improve code styling and remove unused imports

--- cozy_files.py
+++ cozy_files.py
@@ -1,20 +1,16 @@
 import couchmount
-import threading
 import subprocess
 import time
 import sys
 import requests
-from multiprocessing import Process
 import appindicator
 import gtk
-import sys
 import replication
-from couchdb import Database, Document, ResourceNotFound, Server
-from couchdb.client import Row, ViewResults
-try:
-    import simplejson as json
-except ImportError:
-    import json # Python 2.6
+import json
+
+from multiprocessing import Process
+from couchdb import Server
+
 
 
 database = "cozy-files"
@@ -25,11 +21,13 @@ def database_connection():
         server = Server('http://localhost:5984/')
         server.version()
         return server
-    except Exception, e:
+    except Exception:
         time.sleep(5)
+        print('Cannot connect to the database')
         database_connection()
 
 server = Server('http://localhost:5984/')
+
 # Read file
 f = open('/etc/cozy/cozy-files/couchdb.login')
 lines = f.readlines()
@@ -41,62 +39,77 @@ password = lines[1].strip()
 server.resource.credentials = (username, password)
 
 
+### Replication utils ###
+
 def _replicate_to_local(url, pwd, name, idDevice):
     target = 'http://%s:%s@localhost:5984/%s' % (username, password, database)
     url = url.split('/')
     source = "https://%s:%s@%s/cozy" % (name, pwd, url[2])
-    rep = server.replicate(source, target, continuous=True, filter="%s/filter" %idDevice)
+    server.replicate(source, target,
+                     continuous=True, filter="%s/filter" %idDevice)
 
 def _replicate_from_local(url, pwd, name, idDevice):
     source = 'http://%s:%s@localhost:5984/%s' % (username, password, database)
     url = url.split('/')
     target = "https://%s:%s@%s/cozy" % (name, pwd, url[2])
-    rep = server.replicate(source, target, continuous=True, filter="%s/filter" %idDevice)
+    server.replicate(source, target,
+                     continuous=True, filter="%s/filter" %idDevice)
 
 def _one_shot_replicate_to_local(url, pwd, name, idDevice):
     target = 'http://%s:%s@localhost:5984/%s' % (username, password, database)
     url = url.split('/')
     source = "https://%s:%s@%s/cozy" % (name, pwd, url[2])
-    rep = server.replicate(source, target, filter="%s/filter" %idDevice)
+    server.replicate(source, target,
+                     filter="%s/filter" % idDevice)
 
 def _one_shot_replicate_from_local(url, pwd, name, idDevice):
     source = 'http://%s:%s@localhost:5984/%s' % (username, password, database)
     url = url.split('/')
     target = "https://%s:%s@%s/cozy" % (name, pwd, url[2])
-    rep = server.replicate(source, target, filter="%s/filter" %idDevice)
-    
+    server.replicate(source, target,
+                     filter="%s/filter" % idDevice)
+
+### Widget ###
+
 class Menu():
-    def __init__(self, fuse, repli): 
+    def __init__(self, fuse, repli):
         db = server[database]
 
-        self.ind = appindicator.Indicator (
+        self.ind = appindicator.Indicator(
                                   "cozy-files",
                                   "%s/icon/icon.png" % path_cozy,
                                   appindicator.CATEGORY_APPLICATION_STATUS)
         self.ind.set_status (appindicator.STATUS_ACTIVE)
         self.ind.set_attention_icon ("%s/icon/icon.png" %path_cozy)
+
         # create a menu
         self.menu = gtk.Menu()
+
         # Add line to open cozy-files folder
         folder = gtk.MenuItem("Ouvrir le fichier cozy-files")
         self.menu.append(folder)
         folder.show()
+
         # Add line to start synchronisation
         sync = gtk.MenuItem("Forcer une synchronisation")
         self.menu.append(sync)
         sync.show()
+
         # Add line to stop automatic synchronisation
         stop = gtk.MenuItem("Stopper la synchronisation automatique")
         self.menu.append(stop)
         stop.show()
+
         # Add line to start autmotic synchronisation
         autoSync = gtk.MenuItem("Redemarrer la synchronisation automatique")
         self.menu.append(autoSync)
         #autoSync.show()
+
         # Add line for preferences
         preferences = gtk.MenuItem("Preferences...")
         self.menu.append(preferences)
         preferences.show()
+
         # Add line to quit cozy-files
         quit = gtk.MenuItem("Quitter cozy-files")
         self.menu.append(quit)
@@ -106,14 +119,14 @@ class Menu():
         self.menu.show()
         self.ind.set_menu(self.menu)
 
-        def _recover_path(): 
+        def _recover_path():
             res = db.view("device/all")
             if not res:
                 time.sleep(5)
-                return _recover_path(db)
+                return _recover_path()
             else:
                 for device in res:
-                    if not device.value["folder"]: 
+                    if not device.value["folder"]:
                         time.sleep(5)
                         return _recover_path(db)
                     else:
@@ -125,15 +138,19 @@ class Menu():
            subprocess.Popen(["xdg-open", path])
 
         def stopSync(item):
+
             # Stop binary synchronisation
             repli.terminate()
+
             # Stop database replication
             r = requests.get('http://localhost:5984/_active_tasks')
             replications = json.loads(r.content)
             for rep in replications:
                 idRep =  str(rep["replication_id"])
                 data = {"replication_id":"%s" % idRep, "cancel": True}
-                r = requests.post("http://localhost:5984/_replicate", data=json.dumps(data) , headers={'Content-Type': 'application/json'})
+                r = requests.post("http://localhost:5984/_replicate",
+                                  data=json.dumps(data) ,
+                                  headers={'Content-Type': 'application/json'})
             stop.hide()
             autoSync.show()
 
@@ -150,7 +167,8 @@ class Menu():
                 _one_shot_replicate_from_local(url, pwd, name, idDevice)
 
 
-        def startAutoSync(item):             
+        def startAutoSync(item):
+
             # Start metadata replication
             res = db.view("device/all")
             for device in res:
@@ -161,14 +179,18 @@ class Menu():
                 idDevice = device['_id']
                 _replicate_to_local(url, pwd, name, idDevice)
                 _replicate_from_local(url, pwd, name, idDevice)
-            # Start binaries synchronisation      
+
+            # Start binaries synchronisation
             repli = Process(target = replication.main)
             repli.start()
             stop.show()
             autoSync.hide()
 
         def pref(item):
-            config = subprocess.call(['python','%s/windows/preferences_window.py' % path_cozy])
+            subprocess.call([
+                'python',
+                '%s/windows/preferences_window.py' % path_cozy
+            ])
 
         def exit(item):
             # Stop fuse and replication
@@ -192,9 +214,9 @@ def start_prog():
     # Start fuse
     fuse = Process(target = couchmount.main)
     fuse.start()
-    
+
     # Start menu
-    indicator = Menu(fuse, repli)
+    Menu(fuse, repli)
     gtk.main()
 
     #print threading.currentThread()
@@ -204,11 +226,13 @@ def start_prog():
 
 
 try:
+
     server = database_connection()
     server = Server('http://localhost:5984/')
     db = server[database]
-    r = requests.get('http://localhost:5984/_active_tasks')
-    replications = json.loads(r.content)
+    request = requests.get('http://localhost:5984/_active_tasks')
+    replications = json.loads(request.content)
+
     if len(replications) is 0:
         res = db.view("device/all")
         for device in res:
@@ -219,18 +243,33 @@ try:
             idDevice = device['_id']
             _replicate_to_local(url, pwd, name, idDevice)
             _replicate_from_local(url, pwd, name, idDevice)
-    # Start binaries synchronisation      
+
+    # Start binaries synchronisation
     repli = Process(target = replication.main)
     repli.start()
     start_prog()
+
+
+
 except Exception, e:
-    config = subprocess.call(['python','%s/windows/configuration_window.py' % path_cozy])
+
+    config = subprocess.call([
+        'python',
+        '%s/windows/configuration_window.py' % path_cozy
+    ])
+
     if config is 0:
         repli = Process(target = replication.main)
         repli.start()
-        binaries_download = subprocess.call(['python','%s/windows/binaries_download.py' % path_cozy])
+        binaries_download = subprocess.call([
+            'python',
+            '%s/windows/binaries_download.py' % path_cozy
+        ])
         if binaries_download is 0:
-            end = subprocess.call(['python','%s/windows/end_configuration.py' % path_cozy])
+            end = subprocess.call([
+                'python',
+                '%s/windows/end_configuration.py' % path_cozy
+            ])
             start_prog()
     else:
         sys.exit(1)
