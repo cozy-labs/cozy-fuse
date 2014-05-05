f48b17059119d6879e535c26ce8dd52e7adb23e4
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-05-05 12:22:00 +0200

    fix indentation

--- cozyfuse/remote.py
+++ cozyfuse/remote.py
@@ -64,6 +64,7 @@ def remove_device(url, device_id, password):
     Remove device from its Cozy.
     '''
     response = requests.delete('%s/device/%s/' % (url, device_id),
-                    auth=('owner', password), verify=False)
+                               auth=('owner', password), verify=False)
+
     logger.info('[Remote config] Device deletion succeeded for %s.' % url)
     return response
