a2c5fba66afe79f973db274640e7235a2580afea
kload <kload@kload.fr>
2014-07-21 08:10:40 +0000

    [enh] Autocompletion of actions and devices + naming fix

--- cozy-fuse/__main__.py
+++ cozy-fuse/__main__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import argparse
+import argcomplete
 import sys
 
 import actions
+import local_config
 
 from argparse import RawTextHelpFormatter
+from yaml import load
 
 
 # Implement ArgumentParser class to display help on error
@@ -14,46 +17,47 @@ class DefaultHelpParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
-
+def DeviceCompleter(prefix, **kwargs):
+    '''
+    Autocomplete device name
+    '''
+    try:
+        config = local_config.get_full_config()
+    except:
+        return ()
+    else:
+        return (name for name, conf in config.items())
+    
 def main(argv=sys.argv):
 
     parser = DefaultHelpParser(
-        description='Manage your local configuration for Cozy syncing and '
-                    'FUSE mounting',
+        description='Manage your local configuration for Cozy syncing'\
+                    ' and FUSE mounting',
         formatter_class=RawTextHelpFormatter)
 
     subparsers = parser.add_subparsers()
 
     # "configure" action
-    parser_configure = subparsers.add_parser(
-        'configure',
-        help='Configure a new Cozy locally and register current'
-             ' device remotely.'
-    )
+    parser_configure = subparsers.add_parser('configure',
+              help='Configure a new Cozy locally and register current'\
+                   ' device remotely.')
     parser_configure.set_defaults(func=actions.configure_new_device)
-    parser_configure.add_argument(
-        'url',
-        help='URL of remote Cozy to sync'
-    )
-    parser_configure.add_argument(
-        'name',
-        help='Name to choose to reference the synchronized device'
-    )
-    parser_configure.add_argument(
-        'path',
-        help='Local path to choose where Cozy files will be '
-             'mounted (must be an existing directory)'
-    )
+
+    parser_configure.add_argument('url',
+              help='URL of remote Cozy to sync')
+    parser_configure.add_argument('device',
+              help='Device name to choose to refer to the synchronized device')
+    parser_configure.add_argument('path',
+              help='Local path to choose where Cozy files will be mounted'\
+                   ' (must be an existing directory)')
 
     # "sync" action
-    parser_sync = subparsers.add_parser(
-        'sync',
-        help='Synchronize current device with its remote Cozy.'
-    )
+    parser_sync = subparsers.add_parser('sync',
+              help='Synchronize current device with its remote Cozy.')
     parser_sync.set_defaults(func=actions.sync)
 
-    parser_sync.add_argument('name', nargs='*',
-                     help='Name of the device to sync')
+    parser_sync.add_argument('devices', nargs='*',
+              help='Name of devices to sync').completer = DeviceCompleter
 
     # "unsync" action
     parser_kill = subparsers.add_parser(
@@ -63,14 +67,13 @@ def main(argv=sys.argv):
     parser_kill.set_defaults(func=actions.kill_running_replications)
 
     # "mount" action
-    parser_mount = subparsers.add_parser(
-        'mount',
-        help='mount folder for current device.'
-    )
+    parser_mount = subparsers.add_parser('mount',
+              help='mount folder for current device.')
     parser_mount.set_defaults(func=actions.mount_folder)
 
-    parser_mount.add_argument('name', nargs='*',
-                     help='Name of the synchronized device to mount')
+    parser_mount.add_argument('devices', nargs='*',
+              help='Name of synchronized devices'\
+                   ' to mount').completer = DeviceCompleter
 
     # "unmount" action
     parser_unmount = subparsers.add_parser(
@@ -79,26 +82,29 @@ def main(argv=sys.argv):
     )
     parser_unmount.set_defaults(func=actions.unmount_folder)
 
-    parser_unmount.add_argument('name', nargs='*',
-                     help='Name of the synchronized device to unmount')
+    parser_unmount.add_argument('devices', nargs='*',
+              help='Name of the synchronized device'\
+                   ' to unmount').completer = DeviceCompleter
     parser_unmount.add_argument('-p', '--path',
-                     help='Path to unmount')
+              help='Path to unmount')
 
     # "set_default" action
     parser_mount = subparsers.add_parser('set_default',
-                     help='synchronize and mount device at startup')
+              help='synchronize and mount device at startup')
     parser_mount.set_defaults(func=actions.set_default)
 
-    parser_mount.add_argument('name',
-                     help='Name of the device to synchronize and mount at startup')
+    parser_mount.add_argument('device',
+              help='Name of the device to synchronize'\
+                   '  and mount at startup').completer = DeviceCompleter
 
     # "unset_default" action
     parser_mount = subparsers.add_parser('unset_default',
-                     help='avoid device synchronization and mounting at startup')
+              help='avoid device synchronization and mounting at startup')
     parser_mount.set_defaults(func=actions.unset_default)
 
-    parser_mount.add_argument('name', nargs='*',
-                     help='Name of the synchronized device to remove from startup')
+    parser_mount.add_argument('devices', nargs='*',
+              help='Name of synchronized devices to remove'\
+                   ' from startup').completer = DeviceCompleter
 
     # "display_config" action
     parser_display_conf = subparsers.add_parser(
@@ -108,15 +114,12 @@ def main(argv=sys.argv):
     parser_display_conf.set_defaults(func=actions.display_config)
 
     # "remove_config" action
-    parser_rmconf = subparsers.add_parser(
-        'remove_config',
-        help='Remove device from local and remote configuration'
-    )
+    parser_rmconf = subparsers.add_parser('remove_config',
+              help='Remove device from local and remote configuration')
     parser_rmconf.set_defaults(func=actions.remove_device)
-    parser_rmconf.add_argument(
-        'name',
-        help='Name of the synchronized device'
-    )
+
+    parser_rmconf.add_argument('device',
+              help='Name of the synchronized device').completer = DeviceCompleter
 
     # "reset" action
     parser_reset = subparsers.add_parser(
@@ -125,6 +128,9 @@ def main(argv=sys.argv):
              'current device remotely.'
     )
     parser_reset.set_defaults(func=actions.reset)
+
+    # Initialize autocompletion
+    argcomplete.autocomplete(parser)
 
     # Parse CLI arguments and execute related function
     args = parser.parse_args()
--- cozy-fuse/actions.py
+++ cozy-fuse/actions.py
@@ -88,7 +88,7 @@ def kill_running_replications():
             print 'Replication %s was not stopped.' % data['replication_id']
 
 
-def remove_device(name):
+def remove_device(device):
     '''
     Remove device from local and remote configuration by:
 
@@ -97,17 +97,17 @@ def remove_device(name):
     * Removing device from configuration file.
     * Destroying corresponding DB.
     '''
-    (url, path) = local_config.get_config(name)
+    (url, path) = local_config.get_config(device)
 
     couchmount.unmount(path)
-    remove_device_remotely(name)
+    remove_device_remotely(device)
 
     # Remove database
-    dbutils.remove_db(name)
-    dbutils.remove_db_user(name)
+    dbutils.remove_db(device)
+    dbutils.remove_db_user(device)
 
-    local_config.remove_config(name)
-    print 'Configuration %s successfully removed.' % name
+    local_config.remove_config(device)
+    print 'Configuration %s successfully removed.' % device
 
 
 def reset():
@@ -135,14 +135,12 @@ def reset():
     print '[reset] Configuration files deleted, folder unmounted.'
 
 
-def mount_folder(name=[]):
+def mount_folder(devices=[]):
     '''
     Mount folder linked to given device.
     '''
-    if len(name) == 0:
+    if len(devices) == 0:
         devices = local_config.get_startup_devices()
-    else:
-        devices = name
     
     for name in devices:
         try:
@@ -153,14 +151,12 @@ def mount_folder(name=[]):
             unmount_folder(name)
 
 
-def unmount_folder(name=[], path=None):
+def unmount_folder(devices=[], path=None):
     '''
     Unmount folder linked to given device.
     '''
-    if len(name) == 0:
+    if len(devices) == 0:
         devices = local_config.get_startup_devices()
-    else:
-        devices = name
     
     for name in devices:
         if path is None:
@@ -168,23 +164,21 @@ def unmount_folder(name=[], path=None):
         couchmount.unmount(path)
 
 
-def set_default(name):
+def set_default(device):
     '''
     Set configuration parameter for the given device, to synchronize
     and mount it at startup.
     '''
-    local_config.set_startup_config(name, True)
+    local_config.set_startup_config(device, True)
 
 
-def unset_default(name=[]):
+def unset_default(devices=[]):
     '''
     Remove configuration parameter for the given device, to avoid
     synchronization and mounting at startup
     '''
-    if len(name) == 0:
+    if len(devices) == 0:
         devices = local_config.get_startup_devices()
-    else:
-        devices = name
 
     for name in devices:
         local_config.set_startup_config(name, False)
@@ -202,26 +196,26 @@ def display_config():
         print ' '
 
 
-def unregister_device(name):
+def unregister_device(device):
     '''
     Remove device from local configuration, destroy corresponding database
     and unregister it from remote Cozy.
     '''
-    (url, path) = local_config.get_config(name)
-    (device_id, device_password) = local_config.get_device_config(name)
+    (url, path) = local_config.get_config(device)
+    (device_id, device_password) = local_config.get_device_config(device)
 
-    print 'Cozy connection removal for %s.' % name
-    local_config.remove(name)
+    print 'Cozy connection removal for %s.' % device
+    local_config.remove(device)
     print '- Local configuration removed.'
-    dbutils.remove_db(name)
+    dbutils.remove_db(device)
     print '- Local files removed.'
     password = getpass.getpass('Please type the password of your Cozy:\n')
     remote.remove_device(url, device_id, password)
     print '- Remote configuration removed.'
-    print 'Removal succeeded, everything clean!' % name
+    print 'Removal succeeded, everything clean!' % device
 
 
-def configure_new_device(name, url, path):
+def configure_new_device(device, url, path):
     '''
     * Create configuration for given device.
     * Create database and init CouchDB views.
@@ -231,30 +225,28 @@ def configure_new_device(name, url, path):
     print 'Welcome to Cozy Fuse!'
     print ''
     print 'Let\'s go configuring your new Cozy connection...'
-    (db_login, db_password) = dbutils.init_db(name)
+    (db_login, db_password) = dbutils.init_db(device)
     local_config.add_config(name, url, path, db_login, db_password)
     print 'Step 1 succeeded: Local configuration created'
-    register_device_remotely(name)
+    register_device_remotely(device)
     print 'Step 2 succeeded: Device registered remotely.'
     print ''
     print 'Now running the first time replication (it could be very long)...'
-    init_replication(name)
+    init_replication(device)
     print 'Step 3 succeeded: Metadata copied.'
     print ''
-    print 'Cozy configuration %s succeeded!' % name
-    print 'Now type "cozy-fuse sync %s" to keep your data synchronized.' % name
+    print 'Cozy configuration %s succeeded!' % device
+    print 'Now type "cozy-fuse sync %s" to keep your data synchronized.' % device
     print 'And type "cozy-fuse mount %s" to see your files in your ' \
-          'filesystem.' % name
+          'filesystem.' % device
 
 
-def sync(name=[]):
+def sync(devices=[]):
     '''
     Run continuous synchronization between CouchDB instances.
     '''
-    if len(name) == 0:
+    if len(devices) == 0:
         devices = local_config.get_startup_devices()
-    else:
-        devices = name
     
     for name in devices:
         (url, path) = local_config.get_config(name)
